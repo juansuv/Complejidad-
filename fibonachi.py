09d7e547cd5e2cbebde8638e6af77ab1cb25a2a2
plataforma\jdsuarez <jdsuarez@logyca.com>
2021-11-25 21:28:54 -0500

    actualiza

--- fibonachi.py
+++ fibonachi.py
@@ -1,8 +1,22 @@
 def fib(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1    
-    return fib(n-1) + fib(n-2)
+    if n == 0:                      #o(1)
+        return 0                    #o(1)
+    if n == 1:                      #o(1)
+        return 1                    #o(1)
+    return fib(n-1) + fib(n-2)      #o(6n+6n)
 
 print(fib(10))
+
+    #  1  n=1 o n=2 
+    #  t(n-1)+t(n-2)  n>2
+
+    # t(n-1)+t(n-2)  <= 2*t(n-1)
+    #  t(n) = 2*t(n-1)
+    #  t(n-1) = 2*t(n-2)
+    #  t(n-2) = 2*t(n-3)
+    #  t(n-3) = 2*t(n-4)
+    #  t(n) = 2•t(n-1)+2•2•t(n-2)+2•2•2•t(n-3)++2•2•2•2•t(n-4)
+    # sum 2^i•t(n-i)
+    # 2^n•t(n)
+    # o(2^n)
+    
--- multiplicacionmatriz.py
+++ multiplicacionmatriz.py
@@ -2,26 +2,30 @@ from random import randrange
 from llenamatriz import llena_matriz
 
 """
-X = [[12,7,3],
-    [4 ,5,6],
+X = [[12,7,3],         # o(1)
+    [4 ,5,6],           
     [7 ,8,9]]
-Y = [[5,8,1,2],
+Y = [[5,8,1,2],         # o(1)  
     [6,7,3,0],
     [4,5,9,1]]
 """    
-X = llena_matriz(3,3)
-Y = llena_matriz(3,4)
+X = llena_matriz(3,3)    # o(n^2)
+Y = llena_matriz(3,4)    # o(n^2)
 
 # 3x4 matrix
 
 
 def mult_matriz(X,Y):   
-    result = [[0] * len(Y[0]) for i in range(len(X))]     
-    for i in range(len(X)):    
-        for j in range(len(Y[0])):            
-            for k in range(len(Y)):
-                result[i][j] += X[i][k] * Y[k][j]    
-    return result
+    result = [[0] * len(Y[0]) for i in range(len(X))]           #o(n^2)
+    for i in range(len(X)):                                     # o(ñ) * o(n) * o(m)
+        for j in range(len(Y[0])):                              # o(n)
+            for k in range(len(Y)):                             # o(m)
+                result[i][j] += X[i][k] * Y[k][j]               # o(1) 
+    return result                                               # o(1)
 
 
 print(mult_matriz(X,Y))
+
+ #max(o(n^2),o(n),o(1),o(n^3))=o(n^3)
+ # 3n^2 + 2(n*m*ñ) + 2
+ 
